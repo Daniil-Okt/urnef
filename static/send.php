408cc4f5120e5e0289731074e72ba5e1cece3d31
Daniil-Okt <danikoktysyk@gmail.com>
2023-07-14 23:43:10 +0300

    Updates

--- static/send.php
+++ static/send.php
@@ -2,6 +2,18 @@
    $site = 'URNEFP';
    $name = $_POST['name'];
    $phone = $_POST['tel'];
+   $mail = $_POST['mail'];
+   $radioStatus = $_POST['radioStatus'];
+   $radioСontract = $_POST['radioСontract'];
+   $radioDocument = $_POST['radioDocument'];
+   $textSum = $_POST['textSum'];
+   $radioRegistration = $_POST['radioRegistration'];
+   $textCompany1 = $_POST['textCompany1'];
+   $textCompany2 = $_POST['textCompany2'];
+   $checkboxConnect1 = $_POST['checkboxConnect1'];
+   $checkboxConnect2 = $_POST['checkboxConnect2'];
+   $checkboxConnect3 = $_POST['checkboxConnect3'];
+  
    //Отправка в Telegram
   
    $token = "6175032800:AAEjE6ZY2gFttQ6RC4peVyi7mou72IHciA4";
@@ -13,6 +25,16 @@
   $message = "Заявка с сайта: $site\n";
   $message .= "Имя пользователя: $name\n";
   $message .= "Телефон: $phone\n";
+  $message .= "E-mail: $mail\n";
+  $message .= "__ОТВЕТЫ НА ВОПРОССЫ КВИЗА:__";
+  $message .= "Статус дела: $radioStatus\n";
+  $message .= "Между сторонами заключен договор: $radioСontract\n";
+  $message .= "Закрывающие документы: $radioDocument\n";
+  $message .= "Сумма задолженности: $textSum\n";
+  $message .= "Компания зарегистрирована: $radioRegistration\n";
+  $message .= "ИНН компании: $textCompany1\n";
+  $message .= "ИНН компании контрагента: $textCompany2\n";
+  $message .= "Хочу что бы со мной связались: $checkboxConnect1, $checkboxConnect2, $checkboxConnect3";
   // Добавьте еще необходимые поля в сообщение, если нужно
   
   // Отправка запроса в Телеграм
@@ -40,8 +62,6 @@
     // Успешная отправка
     echo "Заявка успешно отправлена в Телеграм.";
   }
-
-
 
 
 // Отображение результата
